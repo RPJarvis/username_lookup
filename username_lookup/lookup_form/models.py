58baf164c78fe8eee9c49fb48aed3c10e1bc99b9
JARVIS <RJarvis1@cnm.edu>
2015-07-21 16:03:01 -0600

    uhhuh

--- username_lookup/lookup_form/models.py
+++ username_lookup/lookup_form/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django import forms
 
+class Username_Query(models.Model):
+    id = forms.IntegerField(max_value=999999999)
+    birthdate = forms.IntegerField(max_value=999999)
+
+    class Meta:
+        fields = ('id', 'birthdate')
 # Create your models here.
--- username_lookup/lookup_form/views.py
+++ username_lookup/lookup_form/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from .models import Username_Query
 # Create your views here.
 
 def index(request):
     if request.method == 'GET':
-        template = loader.get_template('lookup_form/index.html')
-        context = RequestContext(request, {})
+        form = Username_Query()
+        context = {'form': form}
 
-        return HttpResponse(template.render(context))
+        return render(request, 'lookup_form/index.html', context)
 
     elif request.method == 'POST':
         template = loader.get_template('lookup_form/index.html')
