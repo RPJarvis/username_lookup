6c0613e3b96bfb3a828f7354daed5644b72a9295
JARVIS <RJarvis1@cnm.edu>
2015-07-21 15:19:57 -0600

    make a form now

--- username_lookup/lookup_form/views.py
+++ username_lookup/lookup_form/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 # Create your views here.
+
+def index(request):
+    if request.method == 'GET':
+        template = loader.get_template('lookup_form/index.html')
+        context = RequestContext(request, {})
+
+        return HttpResponse(template.render(context))
+
+    elif request.method == 'POST':
+        template = loader.get_template('lookup_form/index.html')
+        context = RequestContext(request, {})
+
+        return HttpResponse(template.render(context))
+
+
