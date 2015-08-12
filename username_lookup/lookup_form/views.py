bd46f263db2c9de7eefb3f5b0b49a4c451c5a3ad
JARVIS <RJarvis1@cnm.edu>
2015-08-12 10:48:06 -0600

    added staff ou search ability

--- username_lookup/lookup_form/views.py
+++ username_lookup/lookup_form/views.py
@@ -4,10 +4,7 @@ import ldap
 from .forms import Username_Query_Form
 from lookup_form import common
 
-            #062979
-            #010037754
-            #print(results[beg_flag:end_flag])
-            #010482287
+#010482287
 
 def index(request):
     context = RequestContext(request)
@@ -17,12 +14,20 @@ def index(request):
         if form.is_valid():
             user_birthdate = str(form.cleaned_data['birthdate'])
             user_id = str(form.cleaned_data['id'])
-            results = str(search(build_query(user_birthdate, user_id)))
-
+            check_staff = True
+            results = str(search(build_query(user_birthdate, user_id), check_staff))
             if results == '[]':
-                result = 'No results found based on what you entered. Try again or call the ITS Service Desk at (505) ' \
-                         '224-4357.'
-                context_dict = {'form': form, 'result': result}
+                check_staff = False
+                results = str(search(build_query(user_birthdate, user_id), check_staff))
+                result = ''
+                if results == '[]':
+                    result = 'No results found based on what you entered. Try again or call the ITS Service Desk at (505) ' \
+                             '224-4357.'
+                beg_flag = results.find('mail') + 9
+                end_flag = results.find('@cnm.edu')
+                form_error = form.errors.as_data()
+                result = 'Your username is: ' + results[beg_flag:end_flag]
+                context_dict = {'form': form, 'result': result, 'error': form_error}
 
                 return render_to_response('lookup_form/index.html', context_dict, context)
 
@@ -53,12 +58,15 @@ def build_query(user_birthdate, user_id):
     return final_query
 
 
-def search(query):
+def search(query, check_staff):
     l = ldap.initialize(common.ldap_url)
     l.simple_bind_s(common.ad_user, common.ad_p)
     scope = ldap.SCOPE_SUBTREE
     attributes = common.attributes
-    base = common.base
+    if check_staff:
+        base = common.staff_base
+    else:
+        base = common.base
     ldap_filter = query
 
     results = l.search_s(base, scope, ldap_filter, attributes)
